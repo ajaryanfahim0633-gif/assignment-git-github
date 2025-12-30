8d0223d24f37dad43a9b4025865b2184c676c3cc
main <ajaryanfahim0633@gmail.com>
2025-12-30 21:26:32 +0600

    first commit

---
+++ Assignment_01.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Creating Set A
+  Set<int> setA = {1, 2, 3, 4, 5};
+  // Creating Set B
+  Set<int> setB = {4, 5, 6, 7, 8};
+  // Printing elements of Set A using for loop
+  print("Elements of Set A:");
+  for (int element in setA) {
+    print(element);
+  }
+  // Printing elements of Set B using for loop
+  print("\nElements of Set B:");
+  for (int element in setB) {
+    print(element);
+  }
+}
+
